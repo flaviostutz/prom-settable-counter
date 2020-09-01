70b488642e76eb9efbae7604cd730a78e472218e
flaviostutz <flaviostutz@gmail.com>
2020-08-31 23:36:27 -0300

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# prom-settable-counter
-Prometheus counter collector with option to set value directly. Useful when getting counters from another system. Use wisely.
+# promcollectors
+
+Prometheus utility collectors that could not be found in official lib, like a counter in which you can issue a "set" value.
+
+The only component here is the settablecounter now. Useful when getting counters from another system. Use wisely because counters are meant to be always going UP and go to zero when resetting it. Don't use this for gauge data!
+
+Thanks https://www.robustperception.io/setting-a-prometheus-counter
+
+## Usage
+
+```golang
+
+    import "github.com/flaviostutz/promcollectors"
+...
+
+	hostInfo = promcollectors.NewSettableCounterVec(prometheus.Opts{
+		Name: "mongo_server_uptime_seconds",
+		Help: "Basic server info and uptime in seconds",
+	}, []string{
+		"host",
+		"version",
+		"process",
+    })
+    
+	prometheus.MustRegister(hostInfo)
+
+    hostInfo.Set(6534, "host1", "4.4.0", "mongos")
+...
+```
+
+See an example usage at http://github.com/stutzlab/mongo-simple-exporter/server_status.go
+
--- settable.go
+++ settable.go
@@ -1,1 +1,78 @@
-package promsettable
+package promcollectors
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+//SettableCounter Settable counter for Prometheus with labels
+type SettableCounter struct {
+	desc          *prometheus.Desc
+	labelNames    []string
+	labelsCounter map[string]float64
+}
+
+//Describe Prometheus will call this to get metrics description
+func (c *SettableCounter) Describe(ch chan<- *prometheus.Desc) {
+	ch <- c.desc
+}
+
+//Collect Prometheus will call this function to collect metric values
+func (c *SettableCounter) Collect(ch chan<- prometheus.Metric) {
+	for labels, v := range c.labelsCounter {
+		lv := strings.Split(labels, "|")[1:]
+		ch <- prometheus.MustNewConstMetric(
+			c.desc,
+			prometheus.CounterValue,
+			v,
+			lv...,
+		)
+	}
+}
+
+//Set the value for the counter. Should never be less than previous value, only if being reset to zero.
+func (c *SettableCounter) Set(value float64, labelValues ...string) error {
+
+	if len(labelValues) != len(c.labelNames) {
+		return fmt.Errorf("Incorrect number of arguments in labelValues. Expected: %d", len(c.labelNames))
+	}
+
+	//calc key for value in map
+	lvv := calcKey(labelValues)
+
+	//if it is an existing value, check for validity
+	for k, v := range c.labelsCounter {
+		if k == lvv {
+			if value != 0 && (value < v || value < 0) {
+				return fmt.Errorf("Invalid value for counter for labels %s. New value should be greater then last value and > 0", labelValues)
+			}
+		}
+	}
+
+	c.labelsCounter[lvv] = value
+	return nil
+}
+
+//NewSettableCounterVec creates a new counter vector with metrics labels
+func NewSettableCounterVec(opts prometheus.Opts, labelNames []string) *SettableCounter {
+	for _, v := range labelNames {
+		if strings.Contains(v, "|") {
+			panic("Label name cannot contain char '|'")
+		}
+	}
+	return &SettableCounter{
+		desc:          prometheus.NewDesc(opts.Name, opts.Help, labelNames, opts.ConstLabels),
+		labelNames:    labelNames,
+		labelsCounter: make(map[string]float64),
+	}
+}
+
+func calcKey(labelValues []string) string {
+	lvv := ""
+	for _, v := range labelValues {
+		lvv = fmt.Sprintf("%s|%s", lvv, v)
+	}
+	return lvv
+}
